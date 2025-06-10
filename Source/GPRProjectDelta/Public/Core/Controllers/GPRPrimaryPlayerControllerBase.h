2a0a02fa2f2cdc89a26b3abf179851ae2b025afd
SCRASSY <caydenwas@gmail.com>
2025-06-10 13:31:45 +1000

    Version 0.1
    - Added gamemode file
    - Added player character file
    - Added player controller file

---
+++ Source/GPRProjectDelta/Public/Core/Controllers/GPRPrimaryPlayerControllerBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GPRPrimaryPlayerControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GPRPROJECTDELTA_API AGPRPrimaryPlayerControllerBase : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
